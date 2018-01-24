8d2bc7912273ccbfed1ad12ed5e8fd2f5e491f48
lovesimly <lovesimly@126.com>
2018-01-24 17:24:37 +0800

    去除标识

--- src/main/java/net/jcip/examples/SingleThreadWebServer.java
+++ src/main/java/net/jcip/examples/SingleThreadWebServer.java
@@ -14,7 +14,7 @@ import java.net.Socket;
 
 public class SingleThreadWebServer {
     public static void main(String[] args) throws IOException {
-        ServerSocket socket = new ServerSocket(80);
+        ServerSocket socket = new ServerSocket(8083);
         while (true) {
             Socket connection = socket.accept();
             handleRequest(connection);
