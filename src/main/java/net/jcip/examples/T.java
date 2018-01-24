18bf4b71d3f4828b430d0a2c39dceb0373deedae
lovesimly <lovesimly@126.com>
2018-01-24 17:52:25 +0800

    去除标识

--- src/main/java/net/jcip/examples/T.java
+++ src/main/java/net/jcip/examples/T.java
@@ -1,7 +1,13 @@
 package net.jcip.examples;
 
+import org.assertj.core.util.Compatibility;
+
 /**
  * Created by lujingzhong on 2018/1/24.
  */
 public class T {
+
+    public  void test(){
+
+    }
 }
